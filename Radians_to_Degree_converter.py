4308abcc049fcec89a62eaa61259b2f40c7441ca
Laaaa00I <alimzanakmagambetov1@gmail.com>
2026-06-27 20:56:32 +0500

    Update Radians_to_Degree_converter.py

--- Radians_to_Degree_converter.py
+++ Radians_to_Degree_converter.py
@@ -8,18 +8,6 @@ while True:
 
     print(f"{radians} radians = {degrees} degrees.")
 
-    while True:
-        again = input("Would you like to continue?"
-                      "\n1. Yes"
-                      "\n2. No"
-                      "\n").lower()
-        if again in YES_ANSWERS:
-            break
-        elif again in NO_ANSWERS:
-            print("Goodbye!")
-            exit()
-        else:
-            print("Invalid input!")
-            #^^^ This cycle stands for continuing conversions
+    continue_cycle()
             
 #^^^ Main convertion cycle
