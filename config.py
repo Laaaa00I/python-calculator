5a134eb6c47783c19fd8f6241ad0fba0ed4c3e8e
Laaaa00I <alimzanakmagambetov1@gmail.com>
2026-06-28 15:59:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,14 @@ from Operations import *
 
 standard_operations_list = {
     **dict.fromkeys(["1", "sum", "сумма", "сложение", "плюс", "+"], Sum),
-    **dict.fromkeys(["2", "mult", "multiply", "умножить", "произведение", "*"], Mult),
-    **dict.fromkeys(["3", "div", "division", "разделить", "деление", "/"], Div),
-    **dict.fromkeys(["4", "sub", "subtract", "отнять", "вычесть", "-"], Sub),
+    **dict.fromkeys(["2", "sub", "subtract", "отнять", "вычесть", "-"], Sub),
+    **dict.fromkeys(["3", "mult", "multiply", "умножить", "произведение", "*"], Mult),
+    **dict.fromkeys(["4", "div", "division", "разделить", "деление", "/"], Div),
     **dict.fromkeys(["5", "pow", "exponent", "степень", "возвести", "^"], Pow),
     **dict.fromkeys(["6", "root", "корень", "извлечь", "радикал", "√"], Root)
 }
 # ^^^ Standard operations list (NOT UPDATING AUTOMATICALLY)
+#Ru: Лист арефметических операций (НЕ ОБНОВЛЯЕТСЯ АВТОМАТИЧЕСКИ)
 trigonometric_operations_list = {
     **dict.fromkeys(["1", "sin", "sine", "синус"], Sin),
     **dict.fromkeys(["2", "cos", "cosine", "косинус"], Cos),
@@ -17,16 +18,18 @@ trigonometric_operations_list = {
     **dict.fromkeys(["4", "cot", "cotangent", "ctg", "котангенс"], Cot)
 }
 #^^^ Trigonometric operations list (NOT UPDATING AUTOMATICALLY)
+#Ru: Лист тригонометрических операций  (НЕ ОБНОВЛЯЕТСЯ АВТОМАТИЧЕСКИ)
 
 standard_operations = '''
 1. Summarise
-2. Multiply
-3. Divide
-4. Subtract
+2. Subtract
+3. Multiply
+4. Divide
 5. Raise to a degree
 6. Root (y-th root of x)
 '''
 #^^^ Standard operations menu (NOT UPDATING AUTOMATICALLY)
+#Ru: Меню арефметических операций  (НЕ ОБНОВЛЯЕТСЯ АВТОМАТИЧЕСКИ)
 trigonometric_operations = '''
 1. Sin
 2. Cos
@@ -34,6 +37,7 @@ trigonometric_operations = '''
 4. Cot
 '''
 #^^^ Trigonometric operations menu (NOT UPDATING AUTOMATICALLY)
+#Ru: Меню тригонометрических операций (НЕ ОБНОВЛЯЕТСЯ АВТОМАТИЧЕСКИ)
 
 standard_mode = [
     "1", "standard", "стандартный", "обычный"
